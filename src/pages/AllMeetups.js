a74830951df4868acd0df44e1766a5af1375306e
siripods <siripods@gmail.com>
2023-05-29 18:37:18 +0700

    add fetch data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { useState, useEffect} from "react";
 
 const DUMMY_DATA = [
     {
@@ -22,12 +23,82 @@ const DUMMY_DATA = [
   ];
 
 function AllMeetupsPage() {
+  //1st element is the current state snapshot
+  //2nd element is the function for updating state
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  //1st argument is function to execute
+  //2nd argment is array of dependencies that your effect function relies on
+  useEffect(() => {
+    //initial loading state = true
+    setIsLoading(true);
+    //fetch() only executes when React execute this section
+    fetch(
+      'https://react-getting-started-63985-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
+    ).then(response => {
+        //fetch() returns a promise which resolves as soon as it is done
+        //when promise completes, we want to process the response body
+        return response.json();
+    }).then (data=> {
+      //response.json() returns a promise also, so we need another attach then
+      
+      console.log('data in response');
+      console.log(data);
+
+      const meetups = [];
+
+      //assume 'data' from Firebase is key-value pairs, not array of objects
+      //so we need transform data
+      for(const key in data) {
+        /*const meetup = {
+           //let each key be id of object meetup
+          id: key,
+          //spread every field in data
+          //assume each  value in data[] is object with fields title, address, description, image
+          ...data[key]
+        }*/
+
+        const meetup = {
+          //let each key be id of object meetup
+          id: key,
+          //spread every field in data, 
+          //assume each value in data[] is object with fields title, address, description, image
+          title: data[key].title,
+          address: data[key].address,
+          description: data[key].description,
+          image: data[key].image
+        }
+
+        console.log('meetup set ');
+        console.log(meetup);
+        meetups.push(meetup);
+      }
+
+      //fetch() finishes, so set loading state to false
+      setIsLoading(false);
+      
+      //we set data into loadedMeeups
+      setLoadedMeetups(meetups);
+    });
+  }, []);
+  //2nd argment of useEffect is array of dependencies
+  //when value of element in this array change, the effect code executes
+  //if array is empty, then the effect code executes only once when this component is loaded for the first time
+
+  //because javascript does not wait, we need to handle during loading content
+  if(isLoading) {
     return (
-    <section>
-        <div>All Meetups</div>
-        <MeetupList meetups={DUMMY_DATA} />
-    </section>
+      <section><p>Loading...</p></section>
     );
+  }
+
+  return (
+  <section>
+      <div>All Meetups</div>
+      <MeetupList meetups={loadedMeetups} />
+  </section>
+  );
 }
 
 export default AllMeetupsPage;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function NewMeetupPage() {
+    //for manipulate browser history
+    const history = useHistory();
+
     function addMeetupHandler(meetupData) {
         fetch(
             'https://react-getting-started-63985-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json', 
@@ -11,7 +15,10 @@ function NewMeetupPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() => {
+            //when finish, navigate to starting page
+            history.replace('/');
+        });
     }
 
     return (
