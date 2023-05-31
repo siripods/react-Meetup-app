1570ae57cc37206c72d49451eacb339d630bc01a
siripods <siripods@gmail.com>
2023-05-31 13:13:05 +0700

    Add Favorite badge

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,14 +1,27 @@
 import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css'
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-context';
 
 function MainNavigation() {
+
+    const favoriteCtx = useContext(FavoritesContext);
+
     return <header className={classes.header}>
         <div className={classes.logo}>React Meetups</div>
         <nav>
             <ul>
-                <li><Link to='/'>All Meetups</Link></li>
-                <li><Link to='/favorites'>My Favorites</Link></li>
-                <li><Link to='/new-meetup'>New Meetup</Link></li>
+                <li>
+                    <Link to='/'>All Meetups</Link>
+                </li>
+                <li>
+                    <Link to='/favorites'>
+                        My Favorites<span className={classes.badge}>{favoriteCtx.totalFavorites}</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/new-meetup'>New Meetup</Link>
+                </li>
             </ul>
         </nav>
     </header>
